56b1f086ea89d4de653cd59aa130c7439e72b71f
userelda1 <126803579+userelda1@users.noreply.github.com>
2025-12-04 18:59:45 +0100

    Secret project using express.js

--- solution.js
+++ solution.js
@@ -1,33 +1,39 @@
-import inquirer from "inquirer";
-import qr from "qr-image";
-import fs from "fs";
+//The password is ILoveProgramming
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-inquirer
-  .prompt([
-    {
-      message: "Type in your URL: ",
-      name: "URL",
-    },
-  ])
-  .then((answers) => {
-    const url = answers.URL;
-    var qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+const app = express();
+const port = 3000;
 
-    fs.writeFile("URL.txt", url, (err) => {
-      if (err) throw err;
-      console.log("The file has been saved!");
-    });
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
-/* 
-1. The inquirer npm package getS user input.
-2. The qr-image npm package turns the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
+var userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  }
+  next();
+}
+app.use(passwordCheck);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+    //Alternatively res.redirect("/");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
